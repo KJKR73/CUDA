96edfebcd44a0d2df4b54a42d2c0a51d09697803
Karanjot Singh Saggu <kjkr7373@gmail.com>
2024-08-26 12:31:30 -0400

    updated MLP

--- CUDA/mlp.cu
+++ CUDA/mlp.cu
@@ -29,33 +29,48 @@ __global__ void forward_layer(double* input, double* weights, double* bias, doub
     }
 }
 
-__global__ void backward_layer(double* delta, double* delta_all, double* input, double* weights,
-                               double* next_weights, double* bias, int input_size, int output_size,
-                               double lr, bool is_output_layer) {
+__global__ void backward_layer_hidden(double* delta, double* delta_all, double* hidden, double* input, double* weights2,
+                                      double* weights1, double* bias, int input_size, int output_size, int hidden_size,
+                                      double lr) {
     // Get the block size
-    int i = blockIdx.x * blockDim.x * threadIdx.x;
-    if (i < input_size) {
-        // output_size = 1
-        // input_size = 2
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < hidden_size) {
         double error = 0.0;
-        if (is_output_layer) {
-            // Get the error based on the activations
-            error = delta - input;
-        }
-        else {
-            // Get the error based on the layers
-            for (int j = 0; j < output_size; j++) {
-                error += delta_all[j] * next_weights[i * output_size + j];
-            }
+        // Get the error based on the layers
+        for (int j = 0; j < output_size; j++) {
+            error += delta[j] * weights2[idx * output_size + j];
         }
 
         // update the delta with the error and get new weights
-        delta_all[i] = error * sigmoid_derivative(input[i]);
-        bias[i] -= delta_all[i] * lr;
+        delta_all[idx] = error * sigmoid_derivative(hidden[idx]);
+        bias[idx] += delta_all[idx] * lr;
 
         // Run the update equations
-        for (int j = 0; j < output_size; j++) {
-            weights[i] -= lr * delta_all[j];
+        for (int j = 0; j < input_size; j++) {
+            weights1[j * hidden_size + idx] += lr * delta_all[idx] * input[j];
+        }
+    }
+}
+
+__global__ void backward_layer_output(double* output, double* target, double* hidden, double* weights2,
+                                      double* bias2, double* delta_output, int output_size, int hidden_size,
+                                      double lr) {
+    // Get the block size
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < output_size) {
+        double error = 0.0;
+        // Get the error based on the activationsc
+        error = target[idx] - output[idx];
+
+        // update the delta with the error and get new weights
+        delta_output[idx] = error * sigmoid_derivative(output[idx]);
+        bias2[idx] += delta_output[idx] * lr;
+
+
+        // Run the update equations
+        for (int j = 0; j < hidden_size; j++) {
+            weights2[j * output_size + idx] += lr * delta_output[idx] * hidden[j];
+
         }
     }
 }
@@ -69,7 +84,7 @@ class MLP {
     std::vector<std::vector<double>> biases;
 
     // All the pointers
-    double *d_input, *d_output;
+    double *d_input, *d_output, *d_target;
     std::vector<double*> d_layer_inputs;
     std::vector<double*> d_layer_output;
     std::vector<double*> d_deltas;
@@ -117,9 +132,6 @@ class MLP {
                 // Allocate the cuda memeory to all the pointers
                 cudaMalloc(&d_weights[i], weights[i].size() * sizeof(double));
                 cudaMalloc(&d_biases[i], biases[i].size() * sizeof(double));
-
-                std::cout << in_layer_size << " | " << out_layer_size << " | "  << weights[i].size() << std::endl;
-
                 cudaMalloc(&d_layer_inputs[i], in_layer_size * sizeof(double));
                 cudaMalloc(&d_layer_output[i], out_layer_size * sizeof(double));
                 cudaMalloc(&d_deltas[i], out_layer_size * sizeof(double));
@@ -135,6 +147,7 @@ class MLP {
             // Few mem allocation at the last
             int input_size = layer_sizes[0];
             int output_size = layer_sizes[num_layers - 1];
+            cudaMalloc(&d_target, output_size * sizeof(double));
             cudaMalloc(&d_input, input_size * sizeof(double));
             cudaMalloc(&d_output, output_size * sizeof(double));
             cudaMalloc(&d_layer_inputs[num_layers - 1], output_size * sizeof(double));
@@ -142,13 +155,12 @@ class MLP {
             cudaMalloc(&d_deltas[num_layers - 1], output_size * sizeof(double));
         }
 
-        // Define the forward process
+        // Define the forward process CODE IS WORKING AS EXPECTED///
         void forward(const std::vector<double>& input, std::vector<double>& output) { 
             // Allocate the memory
             cudaMemcpy(d_input, input.data(), input.size() * sizeof(double), cudaMemcpyHostToDevice);
             cudaMemcpy(d_layer_inputs[0], d_input, input.size() * sizeof(double), cudaMemcpyDeviceToDevice);
 
-            cudaError_t error = cudaGetLastError();
             // Loop and do forward pass
             for (int i = 0; i < num_layers - 1; i++) {
                 int input_size = layer_sizes[i];
@@ -162,27 +174,31 @@ class MLP {
             }
 
             // Reize the outputss
+            cudaMemcpy(d_output, d_layer_output[num_layers - 2], layer_sizes[num_layers - 1] * sizeof(double), cudaMemcpyDeviceToHost);
             output.resize(layer_sizes[num_layers - 1]);
-            error = cudaMemcpy(output.data(), d_layer_output[num_layers - 2],
-                               layer_sizes[num_layers - 1] * sizeof(double), cudaMemcpyDeviceToHost);
+            cudaMemcpy(output.data(), d_layer_output[num_layers - 2],
+                       layer_sizes[num_layers - 1] * sizeof(double), cudaMemcpyDeviceToHost);
         }
 
         void backward(const std::vector<double>& targets, int lr) {
             // Allocate memory
-            cudaMemcpy(d_output, targets.data(), targets.size() * sizeof(double), cudaMemcpyHostToDevice);
-
+            cudaMemcpy(d_target, targets.data(), targets.size() * sizeof(double), cudaMemcpyHostToDevice);
+        
             // Backprop the last layer first to get the output deltas
-            backward_layer<<<(layer_sizes[num_layers - 1]) + 255 / 256, 256>>>(d_output, d_deltas[num_layers - 1], d_layer_output[num_layers - 1],
-                                                                               d_weights[num_layers - 1], d_weights[num_layers - 2],
-                                                                               d_biases[num_layers - 1], layer_sizes[num_layers - 2 ],
-                                                                               layer_sizes[num_layers - 1], lr, true);
+            backward_layer_output<<<(layer_sizes[num_layers - 1]) + 255 / 256, 256>>>(d_output, d_target, d_layer_inputs[num_layers - 2],
+                                                                                      d_weights[num_layers - 2], d_biases[num_layers - 2],
+                                                                                      d_deltas[num_layers - 1], layer_sizes[num_layers - 1],
+                                                                                      layer_sizes[num_layers - 2], lr);
+            cudaDeviceSynchronize();
 
             // Pass through the rest of the layers
-            for (int i = num_layers - 2; i >= 0; i--) {
-                backward_layer<<<(layer_sizes[i - 1]) + 255 / 256, 256>>>(d_deltas[i], d_deltas[i - 1], d_output,
-                                                                          d_weights[i - 1], d_layer_inputs[i - 1],
-                                                                          d_biases[i - 1], layer_sizes[i - 2],
-                                                                          layer_sizes[i - 1], lr, false);
+            for (int i = num_layers - 1; i > 1; i--) {
+                backward_layer_hidden<<<(layer_sizes[i - 1]) + 255 / 256, 256>>>(d_deltas[i - 1], d_deltas[i - 2],
+                                                                                 d_layer_output[i - 1], d_layer_output[i - 2],
+                                                                                 d_weights[i - 1], d_weights[i - 2],
+                                                                                 d_biases[i - 1], layer_sizes[i - 2], layer_sizes[i],
+                                                                                 layer_sizes[i - 1], lr);
+                cudaDeviceSynchronize();
             }
         }
 
@@ -202,7 +218,7 @@ class MLP {
 int main() {
     std::cout << "Simple MLP functions......" << std::endl;
     std::cout << "Loading model" << std::endl;
-    std::vector<int> layer_sizes = {7, 2, 2, 1};
+    std::vector<int> layer_sizes = {7, 2, 1};
     std::cout << "Creating model" << std::endl;
     MLP mlp_mod(layer_sizes);
 
@@ -214,7 +230,7 @@ int main() {
         {0, 0, 0, 1, 0, 0, 1},
         {0, 0, 1, 0, 1, 1, 1},
         {0, 0, 0, 1, 1, 0, 1},
-        {1, 0, 0, 1, 1, 1, 1},
+        {1, 0, 0, 1, 1, 1, 0},
     };
 
     std::vector<std::vector<double>> targets = {
@@ -226,6 +242,16 @@ int main() {
         {1},
         {0}
     };
+
+    double learningRate = 0.1;
+
+    for (int epoch = 0; epoch < 100; ++epoch) {
+        for (size_t i = 0; i < inputs.size(); ++i) {
+            std::vector<double> output;
+            mlp_mod.forward(inputs[i], output);
+            mlp_mod.backward(targets[i], learningRate);
+        }
+    }
 
     std::vector<double> output;
     for (const auto& input: inputs) {
